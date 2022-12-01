92da9e7144905a5a013c78acc37fa0e60d1fe6f4
fangzhiheng <fangzhiheng@corp.netease.com>
2022-12-01 15:16:19 +0800

    i9s extension support config yamls

--- internal/config/extension_i9s.go
+++ internal/config/extension_i9s.go
@@ -1,34 +1,31 @@
 package config
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
 	"strings"
+	"sync"
 )
 
-var ViewToExtension = make(map[string][]Extension)
+var (
+	viewToExtension = make(map[string][]Extension)
+	i9sExtLock      = sync.RWMutex{}
+)
 
-// usage:
+func GetViewToExtension() map[string][]Extension {
+	i9sExtLock.RLock()
+	defer i9sExtLock.RUnlock()
 
-//var LocalExtensions = `extensions:
-//- view: istioView
-//  name: rev-reset
-//  command: sh
-//  args:
-//  - "cmd reset--rev $ISTIO_REV"
-//- view: istioView
-//  name: echo
-//  command: sh
-//  args:
-//  - "echo $ISTIO_REV"
-//- view: sidecarView
-//  name: describe
-//  command: sh
-//  args:
-//  - "kubectl describe pods $NAME -n $NAMESPACE"
-//`
+	return viewToExtension
+}
 
-var LocalExtensions = ``
+var i9sExtDir = filepath.Join(K9sHome(), "i9s", "ext")
 
 type Extensions struct {
 	Extension []Extension `yaml:"extensions"`
@@ -45,19 +42,55 @@ func (p Extension) String() string {
 	return fmt.Sprintf("[%s-%s] %s(%s)", p.View, p.Name, p.Command, strings.Join(p.Args, " "))
 }
 
-func init() {
-	LoadExtensions()
+func InitI9sExtension() {
+	loadI9sExtensions()
 }
 
-func LoadExtensions() {
-	var pp Extensions
-	if err := yaml.Unmarshal([]byte(LocalExtensions), &pp); err != nil {
-		return
-	}
-	for _, v := range pp.Extension {
-		if _, ok := ViewToExtension[v.View]; !ok {
-			ViewToExtension[v.View] = make([]Extension, 0)
+func loadI9sExtensions() {
+	pp := &Extensions{}
+
+	if err := filepath.WalkDir(i9sExtDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Error().Err(err).Msgf("load i9s ext: fail to walk %s, skip...", path)
+			return nil
 		}
-		ViewToExtension[v.View] = append(ViewToExtension[v.View], v)
+		if d.IsDir() {
+			return nil
+		}
+
+		bs, err := os.ReadFile(path)
+		if err != nil {
+			log.Error().Err(err).Msgf("load i9s ext: fail to read %s, skip...", path)
+			return nil
+		}
+
+		dec := yaml.NewDecoder(bytes.NewReader(bs))
+		var ext Extension
+
+		for {
+			err := dec.Decode(&ext)
+			if err != nil {
+				if err != io.EOF {
+					log.Error().Err(err).Msgf("load i9s ext: fail to decode from %s, skip...", path)
+				}
+				break
+			}
+			pp.Extension = append(pp.Extension, ext)
+			ext = Extension{}
+		}
+
+		return nil
+	}); err != nil {
+		log.Error().Err(err).Msgf("load i9s ext walk dir %s met err", i9sExtDir)
 	}
+
+	newViewToExtension := make(map[string][]Extension)
+	for _, ext := range pp.Extension {
+		newViewToExtension[ext.View] = append(newViewToExtension[ext.View], ext)
+	}
+	i9sExtLock.Lock()
+	viewToExtension = newViewToExtension
+	i9sExtLock.Unlock()
+
+	log.Info().Msgf("load i9s ext %s get: %+v", i9sExtDir, newViewToExtension)
 }
